a8ffb6e6cea844969c3f3c364d4d17a15679b960
Jean Lincoln <jeanlincol@gmail.com>
2024-07-29 15:28:13 -0300

    teste jean

--- src/use-cases/example/create/index.ts
+++ src/use-cases/example/create/index.ts
@@ -1,6 +1,6 @@
 import { ExampleRepository } from "@domain/repositories/example";
 import { Injectable } from "@nestjs/common";
-
+// testando branch
 @Injectable()
 export class CreateExampleUseCase {
   constructor(private readonly exampleRepository: ExampleRepository) {}
