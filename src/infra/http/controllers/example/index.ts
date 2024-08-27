37f8731c1e47c290e80594e56869643f8da66a5c
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-27 13:31:42 -0300

    chore: move Example responses to its own layer

--- src/infra/http/controllers/example/index.ts
+++ src/infra/http/controllers/example/index.ts
@@ -4,8 +4,8 @@ import { CreateExampleUseCase } from "@application/use-cases/example/create";
 import { FindAllExampleUseCase } from "@application/use-cases/example/find-all";
 import { CreateExampleDto } from "./dtos/create-example";
 import { Example } from "@prisma/client";
-import { FindAllExampleResponses } from "@infra/config/swagger/responses/find-all";
-import { CreateExampleResponse } from "@infra/config/swagger/responses/create";
+import { FindAllExampleResponses } from "@infra/config/swagger/responses/example/find-all";
+import { CreateExampleResponse } from "@infra/config/swagger/responses/example/create";
 
 @ApiTags("Example")
 @Controller("example")
