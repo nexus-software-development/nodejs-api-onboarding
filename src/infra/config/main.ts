d58410c12735521a89dd44ab99699c58f63b6a3e
Matheus Torres <torresmiranda.rj@gmail.com>
2024-08-27 15:37:23 -0300

    chore: allow class-validator to properly validate dtos

--- src/infra/config/main.ts
+++ src/infra/config/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@infra/modules/app";
 import { SwaggerConfig } from "./swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
+
   SwaggerConfig.config(app);
+
   await app.listen(3001);
 }
 bootstrap();
